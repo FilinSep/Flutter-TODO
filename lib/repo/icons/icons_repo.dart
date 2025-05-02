34109a444c9c23bc5a5b96d79582f8f01ecceed4
FilinSep <telsa6993@gmail.com>
2025-05-02 18:07:38 +0300

    Icons Repository

--- lib/repo/icons/icons_repo.dart
+++ lib/repo/icons/icons_repo.dart
@@ -2,11 +2,30 @@ import 'package:flutter/material.dart' show IconData, Icons;
 
 class IconsRepo {
   Map<String, IconData> icons = {
+    'To do': Icons.list,
+    'To read': Icons.book,
+    'To play': Icons.gamepad,
     'Outside': Icons.park,
     'Go to shop': Icons.shopping_cart,
     'To buy': Icons.receipt_long,
-    'To do': Icons.hardware,
+    'To make': Icons.hardware,
     'To work': Icons.work,
     'To drink': Icons.local_drink,
   };
+
+  IconData getIcon(String key) {
+    if (!icons.containsKey(key)) return icons['To do'] as IconData;
+    return icons[key] as IconData;
+  }
+
+  String translateIcon(IconData icon) {
+    if (!icons.containsValue(icon)) return 'To do';
+
+    for (var key in icons.keys) {
+      if (icons[key] == icon) {
+        return key;
+      }
+    }
+    return 'To do';
+  }
 }
